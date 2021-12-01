9b00107ec3981c993bcbe7d201085fc1a8fcf505
Eduardo Paulo <2018.paulo.eduardo@gmail.com>
2021-11-30 21:39:55 -0300

    Adicionar arquivos de projeto.

---
+++ Migrations/20211115190113_SemTabelaProduto.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DIO_CursoMVC.Migrations
+{
+    public partial class SemTabelaProduto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Categoria = table.Column<int>(type: "int", nullable: false),
+                    CategoriaId = table.Column<int>(type: "int", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Quantidade = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Produtos_Categorias_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categorias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_CategoriaId",
+                table: "Produtos",
+                column: "CategoriaId");
+        }
+    }
+}
---
+++ Models/Context.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIO_CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        //Como criamos uma classe Categoria temos qe dizer ao EFF que teremos uma entidade homônima
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            //Ele, o método, é o responsável por configurar o EFF. 
+            //É dentro dele que especificaremos qual o db que iremos usar
+
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;DataBase=DIO_CursoMVC;Integrated Security=True");
+
+        }
+    }
+}
